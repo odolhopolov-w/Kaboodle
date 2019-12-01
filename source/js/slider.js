89be9906d8955000af8a306f039e8920e9aba4ab
Fomax <fomax0forever@gmail.com>
2019-12-01 20:18:50 +0200

    Finish project

---
+++ source/js/slider.js
@@ -0,0 +1,14 @@
+$('.film-card__list').slick({
+    arrows: false,
+    variableWidth: true,
+    infinite: false,
+    slidesToScroll: 3,
+});
+
+$('.film-list').slick({
+    accessibility: true,
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 3,
+    infinite: false,
+});
